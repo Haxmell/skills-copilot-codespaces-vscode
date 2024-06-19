fe9bfb604083a546f19b3dc6485a3887ca26b260
Haxmell <haxmell@hotmail.com>
2024-06-19 17:40:12 -0600

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+// Create server
+var server = app.listen(8080, function(){
+    console.log('Server is listening on port 8080');
+});
+
+// Serve static files
+app.use(express.static('public'));
